d28a023f01ad1acd83dc1a7fd9d5365ae47125a4
OstrichProjects <jonathanost@gmail.com>
2014-06-02 19:33:28 -0400

    finished contact page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+function sendEmail () {
+	var name = $('#first').val() + " " + $('#last').val(),
+		email = $('#email').val(),
+		subject = $('#subject').val(),
+		message = $('#message').val();
+	$.post('https://api.sendgrid.com/api/mail.send.json?api_user=JonathanOst&api_key=hownowbrowncow&to=kiragoldsmith@gmail.com&toname=Kira Goldsmith&subject=' + subject +'&text='+ message +'&from=' + email + '&fromname=' + name);
+}
 
+$('#contact').submit(function () {
+	sendEmail();
+	$('#contact').css('text-align','center');
+	$('#contact').css('font-size', 30);
+	$('#contact').html('Thanks for the email!  I\'ll be in touch.');
+	return false;
+});
